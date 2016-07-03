4b7577f29ef7af5e64d3ff9a6400d9cb18449bcb
theslainsays <kevin.gilreath221@gmail.com>
2016-07-03 12:44:59 -0700

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -72,15 +72,28 @@ plt.show()
 
 # - Findings
 
-#   When I first analyzed data.Montoya.txt, I actually didn't know what rtb.requests were, until I Google'd it.  After learning that rtb stood for Real-time bidding, I understood what the resulting graph was trying to show, but I didn't know what "value" referred to.  My best guess is that it refers to the number of bid requests being made at certain times.  However, there were some negative values in the graph. I can only assume that these negative values represented special moments. They could be: glitchs/errors that occur (these values could be removed and have little effect on the graph as a whole), times where bids have finished and the data center(s) are restarting, or updates that require the data centers to stop until the update is applied. Overall, I learned from the graph that data centers I and S are in the same area (which explains why the graphs are similar, except for size), and data center A simply had a lower amount of activity compared to the other two.
+#   When I first analyzed data.Montoya.txt, I actually didn't know what rtb.requests were, until I Google'd it.  
+#   After learning that rtb stood for Real-time bidding, I understood what the resulting graph was trying to show, 
+#   but I didn't know what "value" referred to.  My best guess is that it refers to the number of bid requests 
+#   being made at certain times.  However, there were some negative values in the graph. I can only assume that these 
+#   negative values represented special moments. They could be: glitchs/errors that occur 
+#   (these values could be removed and have little effect on the graph as a whole), times where bids have finished and the data center(s)
+#   are restarting, or updates that require the data centers to stop until the update is applied. 
+#   Overall, I learned from the graph that data centers I and S are in the same area 
+#   (which explains why the graphs are similar, except for size), and data center A simply had a lower amount of activity 
+#   compared to the other two.
 
 # - Challenges
 
-#   I had one particular challenge: choosing which programming language to use. At first, I was going to use C, but then switched to Python because I stumbled upon a graph-making tutorial on Python while trying to figure out what functions I need to make a graph for C.
+#   I had one particular challenge: choosing which programming language to use. At first, I was going to use C, 
+#   but then switched to Python because I stumbled upon a graph-making tutorial on Python while trying to figure out what 
+#   functions I need to make a graph for C.
 
 # - Conclusion
 
-#   This exercise has been the first time since college that I got to program. I have always feared that I may have forgotten how to program completely, but completing this exercise has taught me otherwise. I only wish I had more clarity about data.Montoya.txt and exactly what it is about. In conclusion, I hope this program demostrates my programming capabilities.
+#   This exercise has been the first time since college that I got to program. I have always feared that I may have forgotten how to 
+#   program completely, but completing this exercise has taught me otherwise. I only wish I had more clarity about data.Montoya.txt 
+#   and exactly what it is about. In conclusion, I hope this program demostrates my programming capabilities.
 
 
 
